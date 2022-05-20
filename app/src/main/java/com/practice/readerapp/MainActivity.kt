c9b2076e2edb07d6cd5d556d6741fa2da546e1d7
Jorge Agullo <agullojorge@gmail.com>
2022-05-20 17:28:01 +0200

    [FEATURE] Added custom textField & apply for emailInput

--- app/src/main/java/com/practice/readerapp/MainActivity.kt
+++ app/src/main/java/com/practice/readerapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.google.firebase.firestore.FirebaseFirestore
@@ -80,3 +81,4 @@ fun DefaultPreview() {
 
     }
 }
+
