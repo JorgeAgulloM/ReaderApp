f13b493de7f6cc93e5146953aa3fd04432b9f612
Jorge Agullo <agullojorge@gmail.com>
2022-05-23 10:48:58 +0200

    [FEATURE] WIP Added Model User

---
+++ app/src/main/java/com/practice/readerapp/model/MUser.kt
@@ -0,0 +1,1 @@
+package com.practice.readerapp.modeldata class MUser(    val id: String?,    val userId: String?,    val displayName: String?,    val avatarUrl: String?,    val quote: String,    val profession: String)
