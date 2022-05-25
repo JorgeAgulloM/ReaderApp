22c6e34a20e66e62950d27ea84c5c6881aee24b6
Jorge Agullo <agullojorge@gmail.com>
2022-05-25 12:04:23 +0200

    [FEATURE] WIP Save Books in Firebase

---
+++ app/src/main/java/com/practice/readerapp/screens/home/HomeScreenViewModel.kt
@@ -0,0 +1,1 @@
+package com.practice.readerapp.screens.homeimport androidx.lifecycle.ViewModelimport com.practice.readerapp.repository.FireRepositoryimport dagger.hilt.android.lifecycle.HiltViewModelimport javax.inject.Inject@HiltViewModelclass HomeScreenViewModel @Inject constructor(private val repository: FireRepository): ViewModel() {}
